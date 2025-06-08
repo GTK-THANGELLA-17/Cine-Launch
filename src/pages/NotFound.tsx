131b560afcb1e6ce4e1707df79aba37f492bcf91
G THANGELLA <161929651+GTK-THANGELLA-17@users.noreply.github.com>
2025-06-09 00:03:53 +0530

    Cine-Launch V1

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,4 @@
+
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 
@@ -8,7 +9,7 @@ const NotFound = () => {
         <h1 className="text-4xl font-display font-bold mb-4 text-gradient-gold">404</h1>
         <p className="text-xl text-white mb-6">Oops! This scene didn't make the final cut.</p>
         <Button asChild className="bg-cinelaunch-gold text-black hover:bg-cinelaunch-gold/90">
-          <Link to="/home">Return to Home</Link>
+          <Link to="/">Return to Home</Link>
         </Button>
       </div>
     </div>
